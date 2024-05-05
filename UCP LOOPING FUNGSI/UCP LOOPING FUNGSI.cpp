47242f1d2921894a03be4e7b7638fe837fabbef1
SandyArdiyan <sanardiyan881@gmail.com>
2024-05-05 11:58:54 +0700

    Add project files.

---
+++ UCP LOOPING FUNGSI/UCP LOOPING FUNGSI.cpp
@@ -0,0 +1,97 @@
+// 6. A
+#include <iostream>
+using namespace std;
+
+int arr[20]
+int n;
+
+void input() {
+	while (true) {
+		cout << " Nasukkan banyaknya kandidat : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "==============" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "==============" << endl;
+
+int nkandidat, nMtk, nBinggiris
+int handidat = 20, hMtk = 70, hBinggiris = 80;
+string nama;
+
+void input()
+{
+	cout << "Masukan Nama"
+		cin >> nama;
+	cout << "Masukan Jumlah kandidat" = ";
+		cin >> nMtk;
+	cout << "Masukan jumlah nilai Mtk = ";
+	cin >> nmtk;
+	cout << "MAsukan Jumlah Nilai Binggiris = ";
+	cin >> nBinggiris;
+}
+
+int hitungnilai()
+{
+	return (nKandidat * hbinggris) + (nMtk * hbinggris) + (nbinggiris * hbinggiris)
+}
+void display()
+{
+	cout << "kandidat = " << kandidat << endl;
+	cout << "jumlah binggris = " << nMtk << endl;
+	cout << "Jumlah Mtk = " << nmtk << endl"
+		cout << "Jumlah binggris = " << nbinggris << endl;
+	cout << "Total Nilai ." << hitungnilai() << endl
+}
+
+// B. 
+ Struct kandidat [
+	 string status;
+	 string nama;
+
+	int main ()[
+		kandidat mahasiswa;
+		cout << "Asroni di Terima :";
+		cin << mhs.diterima;
+		cout << "Nama Pendaftar ";
+		cin << mhs.pendaftar;
+
+// c. 
+
+// 1. Variabel ada 3 macam :
+// - Yang tidak berparameter 
+// Sintax 
+// * Prosuder : void nama prosedur ()
+// * Fungsi	  : tipe data nama Fungsi()
+// > sintax parameter : tipe data nama variabel (int panjang, float nilai)
+// 
+// - Berparameter
+// * Prosuder : void nama prosedur (daftar parameter)
+// > sintax parameter : tipe data nama variabel(int panjang, float nilai)
+//
+// - Switct Case 
+// * pengendalian program , di gunakan saat if else bertumpuk banyak
+// 
+// 2. int penjumlahan(int a,int b)
+// 		{
+// 		   retrun (a +b)
+// 		   }
+// 
+// 3.  struct Mahasiswa [
+//		String nim;
+//		string nama;
+// 
+// int main () [
+//		Mahasiswa Mhs;
+//		cout << " Nomor Nahasiswa : ";
+//		cin >> mhs.nim;
+//		cout << "NAma Mahasiswa : ";
+//		cin >> mhs.nama ;
+//		cin >> mhs.nama ;
+// 
+//	4. 
